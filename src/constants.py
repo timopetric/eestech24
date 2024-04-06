b9a22f5aff113ade3c41949324b0512b9caa2049
Aljaž Grdadolnik <ag5319@student.uni-lj.si>
2024-04-06 11:47:17 +0100

    Dodani števci na planinskih poteh leta 2022, 2023, 2024

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,5 @@
 DATA_GORSKE_0 = '../data/gorske_nesrece/Gorske-nesrece-20221129.csv'
 DATA_GORSKE_1 = '../data/gorske_nesrece/Gorske-od-18.10.2022-dalje.csv'
 
-DATA_GLOB_PLANINSKE = "TODO"
+# will probably need to discard the zdruzeno.csv file
+DATA_GLOB_PLANINSKE_MAKRO = '../data/planinstvo/*.csv'
