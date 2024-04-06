91475088e2499ebf57e484d7df43762a29201183
timopetric <timopetric@hotmail.com>
2024-04-06 13:33:08 +0200

    Add support for planinstvo data for years 2022 and 2023

--- src/constants.py
+++ src/constants.py
@@ -2,4 +2,7 @@ DATA_GORSKE_0 = '../data/gorske_nesrece/Gorske-nesrece-20221129.csv'
 DATA_GORSKE_1 = '../data/gorske_nesrece/Gorske-od-18.10.2022-dalje.csv'
 
 # will probably need to discard the zdruzeno.csv file
-DATA_GLOB_PLANINSKE_MAKRO = '../data/planinstvo/*.csv'
+# for year 2022 and 2023
+DATA_GLOB_PLANINSKE_MAKRO_2022 = '../data/planinstvo/2022/*.csv'
+DATA_GLOB_PLANINSKE_MAKRO_2023 = '../data/planinstvo/2023/*.csv'
+
